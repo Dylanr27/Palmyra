e6aae7d01fc928defe3a5b0d75034ed1ba004618
Dylanr27 <97147146+Dylanr27@users.noreply.github.com>
2024-08-06 20:18:43 -0700

    Delete src/utils directory

--- src/utils/setupSecretKey.js
+++
@@ -1,5 +0,0 @@
-import { generateSecretKey } from './generateSecret.js';
-
-const secretKey = generateSecretKey();
-console.log('Generated SECRET_KEY:', secretKey);
-console.log('Set this key as SESSION_SECRET_KEY in your environment variables.');
