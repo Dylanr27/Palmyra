f02268d151c3166b6b48b116804b67a06c2522dc
Dylanr27 <djregister_50@msn.com>
2024-08-09 17:42:30 -0700

    test: test deployment without login

--- app.js
+++ app.js
@@ -24,31 +24,31 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( express.static( 'public' ) );
 app.set( 'view engine', 'ejs' );
 
-app.use( session( {
-    secret: process.env.SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } // Note: In production, set secure to true
-} ) );
+// app.use( session( {
+//     secret: process.env.SESSION_SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: false } // Note: In production, set secure to true
+// } ) );
 
-app.use( passport.initialize() );
-app.use( passport.session() );
+// app.use( passport.initialize() );
+// app.use( passport.session() );
 
-setupGoogleAuth( app );
+// setupGoogleAuth( app );
 
-function isUserAuthenticated ( req, res, next )
-{
-    if ( req.isAuthenticated() )
-    {
-        return next();
-    }
-    return res.redirect( '/' );
-}
+// function isUserAuthenticated ( req, res, next )
+// {
+//     if ( req.isAuthenticated() )
+//     {
+//         return next();
+//     }
+//     return res.redirect( '/' );
+// }
 
-app.use( '/auth', authRoutes );
-app.use( '/events', isUserAuthenticated, eventRoutes );
-app.use( '/menu-items', isUserAuthenticated, menuItemRoutes );
-app.use( '/submit-feedback', feedbackRouter );
+// app.use( '/auth', authRoutes );
+// app.use( '/events', isUserAuthenticated, eventRoutes );
+// app.use( '/menu-items', isUserAuthenticated, menuItemRoutes );
+// app.use( '/submit-feedback', feedbackRouter );
 
 app.get( '/', async ( req, res ) =>
 {
@@ -57,15 +57,16 @@ app.get( '/', async ( req, res ) =>
         const events = await listEvents();
         const menuItems = await listMenuItems();
 
-        const userIsAuthorized = req.isAuthenticated();
+        // const userIsAuthorized = req.isAuthenticated();
 
-        console.log( 'User is authorized:', userIsAuthorized );
+        // console.log( 'User is authorized:', userIsAuthorized );
 
         res.render( 'index',
             {
                 events: events,
-                menuItems: menuItems,
-                userIsAuthorized: userIsAuthorized
+                menuItems: menuItems
+                // menuItems: menuItems,
+                // userIsAuthorized: userIsAuthorized
             }
         );
     } catch ( error )
@@ -76,17 +77,21 @@ app.get( '/', async ( req, res ) =>
 } );
 
 const port = process.env.PORT || 8080;
+app.listen( port, () =>
+{
+    console.log( `App listening at http://localhost:${ port }` );
+} );
 
-sequelize.authenticate()
-    .then( () =>
-    {
-        console.log( 'Connection has been established successfully.' );
-        app.listen( port, () =>
-        {
-            console.log( `App listening at http://localhost:${ port }` );
-        } );
-    } )
-    .catch( err =>
-    {
-        console.error( 'Unable to connect to the database:', err );
-    } );
+// sequelize.authenticate()
+//     .then( () =>
+//     {
+//         console.log( 'Connection has been established successfully.' );
+//         app.listen( port, () =>
+//         {
+//             console.log( `App listening at http://localhost:${ port }` );
+//         } );
+//     } )
+//     .catch( err =>
+//     {
+//         console.error( 'Unable to connect to the database:', err );
+//     } );
