cc80b61a2447848780f9de565d766293eb1cee23
Dylanr27 <djregister_50@msn.com>
2024-08-14 12:57:32 -0700

    feat: fixed deployment and login

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import { config } from 'dotenv';
-import sequelizeInstance from './src/config/sequelize.js';
 import connectDb from './src/config/database.js';
 import insertData from './scripts/seedDb.js';
 import passport from 'passport';
@@ -27,31 +26,31 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( express.static( 'public' ) );
 app.set( 'view engine', 'ejs' );
 
-// app.use( session( {
-//     secret: process.env.SESSION_SECRET_KEY,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false } // Note: In production, set secure to true
-// } ) );
+app.use( session( {
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Note: In production, set secure to true
+} ) );
 
-// app.use( passport.initialize() );
-// app.use( passport.session() );
+app.use( passport.initialize() );
+app.use( passport.session() );
 
-// setupGoogleAuth( app );
+setupGoogleAuth( app );
 
-// function isUserAuthenticated ( req, res, next )
-// {
-//     if ( req.isAuthenticated() )
-//     {
-//         return next();
-//     }
-//     return res.redirect( '/' );
-// }
+function isUserAuthenticated ( req, res, next )
+{
+    if ( req.isAuthenticated() )
+    {
+        return next();
+    }
+    return res.redirect( '/' );
+}
 
-// app.use( '/auth', authRoutes );
-// app.use( '/events', isUserAuthenticated, eventRoutes );
-// app.use( '/menu-items', isUserAuthenticated, menuItemRoutes );
-// app.use( '/submit-feedback', feedbackRouter );
+app.use( '/auth', authRoutes );
+app.use( '/events', isUserAuthenticated, eventRoutes );
+app.use( '/menu-items', isUserAuthenticated, menuItemRoutes );
+app.use( '/submit-feedback', feedbackRouter );
 
 app.get( '/', async ( req, res ) =>
 {
@@ -63,15 +62,15 @@ app.get( '/', async ( req, res ) =>
         console.log( 'Events:', events );
         console.log( 'Menu Items:', menuItems );
 
-        // const userIsAuthorized = req.isAuthenticated();
+        const userIsAuthorized = req.isAuthenticated();
 
-        // console.log( 'User is authorized:', userIsAuthorized );
+        console.log( 'User is authorized:', userIsAuthorized );
 
         res.render( 'index',
             {
                 events: events,
                 menuItems: menuItems,
-                userIsAuthorized: false
+                userIsAuthorized: userIsAuthorized
             }
         );
     } catch ( error )
--- src/config/googleAuth.js
+++ src/config/googleAuth.js
@@ -4,35 +4,43 @@ import { config } from 'dotenv';
 
 config();
 
-export default function setupGoogleAuth(app) {
+export default function setupGoogleAuth ( app )
+{
 
-    passport.serializeUser((user, done) => {
+    passport.serializeUser( ( user, done ) =>
+    {
         // Serialize the user session with a simple, static identifier
-        done(null, process.env.USER_EMAIL);
-    });
-    
-    passport.deserializeUser((email, done) => {
+        done( null, process.env.USER_EMAIL );
+    } );
+
+    passport.deserializeUser( ( email, done ) =>
+    {
         // Directly return the email as the user object for the session
-        if (email === process.env.USER_EMAIL) {
-            done(null, email);
-        } else {
-            done(new Error("User not found"), null);
+        if ( email === process.env.USER_EMAIL )
+        {
+            done( null, email );
+        } else
+        {
+            done( new Error( "User not found" ), null );
         }
-    });
-    
-    passport.use(new GoogleStrategy({
+    } );
+
+    passport.use( new GoogleStrategy( {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        callbackURL: "https://palmyra-development-d8h2dtcwbedxfec5.eastus2-01.azurewebsites.net/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, cb) => {
+        ( accessToken, refreshToken, profile, cb ) =>
+        {
 
-        console.log("Logging in user's email:", profile.emails[0].value);
+            console.log( "Logging in user's email:", profile.emails[ 0 ].value );
 
-        if (profile.emails && profile.emails[0].value === process.env.USER_EMAIL) {
-            return cb(null, profile);
-        } else {
-            return cb(null, false, { redirectTo: '/unauthorized' });
-        }
-    }));
+            if ( profile.emails && profile.emails[ 0 ].value === process.env.USER_EMAIL )
+            {
+                return cb( null, profile );
+            } else
+            {
+                return cb( null, false, { redirectTo: '/unauthorized' } );
+            }
+        } ) );
 }
